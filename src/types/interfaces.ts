e7a01399c0f5cb7d236a9889ebbb17a801326afb
agomzlo <agomzlodev@gmail.com>
2025-10-16 21:02:45 +0200

    fix: Fixing some errors on events and links

---
+++ src/types/interfaces.ts
@@ -0,0 +1,10 @@
+export interface Event {
+    name: string;
+    dateDescription: string;
+    imgSrc: string;
+    shortDescription: string;
+    description: string;
+    date: string;
+    time: string;
+    slug: string;
+}
